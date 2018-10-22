37b437d189d4851bd243c1035c680459cd8f9b1e
Maarten Nieber <hallomaarten@yahoo.com>
2018-10-22 18:30:39 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='dodo_deploy_commands',
+    version='0.0.1',
+    description='Deployment related Dodo Commands',
+    url='https://github.com/mnieber/dodo_deploy_commands',
+    download_url='https://github.com/mnieber/dodo_deploy_commands/tarball/0.0.1',
+    author='Maarten Nieber',
+    author_email='hallomaarten@yahoo.com',
+    license='MIT',
+    packages=[
+        'dodo_deploy_commands',
+    ],
+    package_data={},
+    entry_points={},
+    install_requires=[],
+    zip_safe=False)
